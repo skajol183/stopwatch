873fcb972390fd7e8cfc1fe034b43cf9bd3e565e
Kajol Singh <skajol183@gmail.com>
2024-06-27 15:55:45 +0530

    added styles

--- src/Components/StopWatch/index.js
+++ src/Components/StopWatch/index.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./index.scss";
 
 const StopWatch = () => {
   return (
-    <div>
-      <h1>Welcome to the Stop Watch App</h1>
+    <div className="app-container">
+      <div className="stopwatch-container">
+        <h1 className="stopwatch">Stopwatch</h1>
+        <div className="timer-container">
+          <div className="timer">
+            <img
+              className="timer-image"
+              src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"
+              alt="stopwatch"
+            />
+            <p className="heading">Timer</p>
+          </div>
+          <h1 className="stopwatch-timer">0</h1>
+          <div className="timer-buttons">
+            <button type="button" className="start-button button">
+              Start
+            </button>
+            <button type="button" className="stop-button button">
+              Stop
+            </button>
+            <button type="button" className="reset-button button">
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StopWatch
+export default StopWatch;
