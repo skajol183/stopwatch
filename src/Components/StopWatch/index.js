50715632766e19758ea1128350ce7fae9c1ede05
Kajol Singh <skajol183@gmail.com>
2024-06-27 16:27:10 +0530

    added functionality

--- src/Components/StopWatch/index.js
+++ src/Components/StopWatch/index.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.scss";
 
 const StopWatch = () => {
+  const [isPause, setIsPause] = useState("Start");
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const handleStartPause = () => {
+    setIsRunning(!isRunning);
+    setIsPause("Pause");
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setTimeout(() => {
+      setElapsedTime(0);
+    }, 2000);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+
+  const getFormattedTime = (time) => {
+    const minutes = Math.floor(time / 60000)
+      .toString()
+      .padStart(2, "0");
+    const seconds = Math.floor((time / 1000) % 60)
+      .toString()
+      .padStart(2, "0");
+    const milliseconds = (time % 1000).toString().padStart(3, "0");
+    return `${minutes}:${seconds}.${milliseconds}`;
+  };
+
   return (
     <div className="app-container">
       <div className="stopwatch-container">
@@ -15,15 +59,29 @@ const StopWatch = () => {
             />
             <p className="heading">Timer</p>
           </div>
-          <h1 className="stopwatch-timer">0</h1>
+          <h1 className="stopwatch-timer">{getFormattedTime(elapsedTime)}</h1>
           <div className="timer-buttons">
-            <button type="button" className="start-button button">
-              Start
+            <button
+              type="button"
+              className="start-button button"
+              onClick={handleStartPause}
+            >
+              {isPause}
             </button>
-            <button type="button" className="stop-button button">
+            <button
+              type="button"
+              className="stop-button button"
+              onClick={handleStop}
+              disabled={!isRunning}
+            >
               Stop
             </button>
-            <button type="button" className="reset-button button">
+            <button
+              type="button"
+              className="reset-button button"
+              onClick={handleReset}
+              disabled={!elapsedTime}
+            >
               Reset
             </button>
           </div>
